fc6363a3f457a2138156a7a610facadaf08b30fd
Diego <diegovalera36@gmail.com>
2025-05-21 22:53:04 -0300

    olvide OrigenGeneracion

---
+++ models/OrigenGeneracion.js
@@ -0,0 +1,7 @@
+class OrigenGeneracion {
+    constructor(descripcion, nombre) {
+        this.descripcion = descripcion;
+        this.nombre = nombre;
+    }
+}
+module.exports = OrigenGeneracion;
